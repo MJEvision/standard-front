4ccecb81df8a7085cf8fe331cd5202b859ca077e
MJE <moon080108@gmail.com>
2025-07-07 23:59:28 +0900

    최종

--- vite.config.js
+++ vite.config.js
@@ -14,14 +14,13 @@ export default defineConfig({
       '/email-verification': {
         target: 'http://3.113.6.231:3000',
         changeOrigin: true,
-        // 필요하면 rewrite도 넣어라, 근데 지금은 안 써도 됨
       },
       '/api': {
         target: 'https://apis.data.go.kr',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
-        secure: false, // https 인증서 문제 있을 때
-        ws: true, // 웹소켓 필요하면
+        secure: false, 
+        ws: true, 
       },
     },
   },
